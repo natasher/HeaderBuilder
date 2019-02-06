32e29815859a6d06730c10f8465744b27b859e99
Artur Woźniak <artur.wozniak69@gmail.com>
2019-02-06 13:18:29 +0100

    Clean scaffold
    Signed-off-by: Artur Woźniak <artur.wozniak69@gmail.com>

--- dev_frontend/src/components/Forms/Modals/ModalSaveHeader.vue
+++ dev_frontend/src/components/Forms/Modals/ModalSaveHeader.vue
@@ -1,46 +1,30 @@
 <template>
-  <modal
-    name          = "MfnGenericModal"
-    @before-open  = "beforeOpen"
-    height        = "auto"
-    :classes      = "[ 'info-modal', 'v--modal' ]"
-    :clickToClose = "closeOnClick" >
+  <transition name="saved-header-info">
+    <modal
+      name          = "ModalSaveHeader"
+      @before-open  = "beforeOpen"
+      height        = "auto"
+      :classes      = "[ 'info-modal', 'v--modal' ]"
+      :clickToClose = "closeOnClick" >
 
-    <div class = "header">
+      <div class = "header">
 
-      <div class = "left">
-        <div class = "title">{{ title }}</div>
+        <div class = "left">
+          <div class = "title"></div>
+        </div>
+
       </div>
 
-    </div>
+      <!-- TODO: content gif -->
 
-    <div class = "content"
-        v-html = "text" ></div>
-
-    <button
-      v-for = "( btn, index ) in buttons"
-      @click = "btn.handler"
-      class = "mfn-button-generic"
-      :key = "index">
-        {{ btn.text }}
-    </button>
-
-  </modal>
+    </modal>
+  </transition>
 </template>
 
 <script>
 export default {
 
   name: 'MfnGenericModal',
-
-  data () {
-    return {
-      title        : '',
-      text         : '',
-      buttons      : [],
-      closeOnClick : false
-    }
-  },
 
   methods: {
     /**
@@ -52,10 +36,7 @@ export default {
      * @param {event} passes grid item related data
      */
     beforeOpen ( event ) {
-      this.title        = event.params.title
-      this.text         = event.params.text
-      this.buttons      = event.params.buttons
-      this.closeOnClick = event.params.closeOnClick
+    /** TODO: możebyć dobre miejsce na leave transition */
     },
   }
 }
